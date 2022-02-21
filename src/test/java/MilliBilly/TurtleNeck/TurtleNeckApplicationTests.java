06a1b64c18f8f436bb350e01b64329cdac98538d
kyoshong <hyospark@student.42seoul.kr>
2022-02-21 12:01:55 +0900

    :tada: feat: init project

---
+++ src/test/java/MilliBilly/TurtleNeck/TurtleNeckApplicationTests.java
@@ -0,0 +1,13 @@
+package MilliBilly.TurtleNeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleNeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
