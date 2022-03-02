a2ede527096b6ac50e4cd1eafa9759e3b4fdb1c6
kyoshong <hyospark@student.42seoul.kr>
2022-03-02 20:07:36 +0900

    merge

--- src/main/java/MilliBilly/TurtleNeck/TurtleNeckApplication.java
+++ src/main/java/MilliBilly/TurtleNeck/TurtleNeckApplication.java
@@ -1,5 +1,6 @@
 package MilliBilly.TurtleNeck;
 
+import MilliBilly.TurtleNeck.controller.ProfileController;
 import MilliBilly.TurtleNeck.entity.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +10,13 @@ public class TurtleNeckApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TurtleNeckApplication.class, args);
-
-
+		ProfileController profile = new ProfileController();
 		Member skim = new Member(1L, "skim", "default" );
 		Member hyospark = new Member(2L, "hyospark", "default" );
 		Member hyoon = new Member(2L, "hyoon", "default" );
+		profile.getProfile(hyoon);
+		profile.getProfile(hyospark);
+		profile.getProfile(skim);
+
 	}
 }
